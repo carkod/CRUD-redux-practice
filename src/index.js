d6c114156dd2d4f417cd4148ff908fc9431c21ba
Carkod <carkodesign@gmail.com>
2017-05-03 11:37:36 +0000

    App and Redux setup (Chap 1)

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import './index.css';
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+      applyMiddleware(thunk)
+    )
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
