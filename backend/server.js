c06d6f23aae8eadc2b519041abe5ec3324ec3f7a
Carkod <carkodesign@gmail.com>
2017-05-06 01:09:34 +0000

    h

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongodb from 'mongodb';
+
+const app = express();
+const dbUrl = 'mongodb://localhost/crudwithredux';
+
+
+mongodb.MongoClient.connect(dbUrl, function(err,db) {
+    app.get('/api/games', (req,res) => {
+        
+        db.collection('games').find({}).toArray((err,games) => {
+           res.json({games});
+        });
+    });
+    const PORT = 8080;
+   app.listen(PORT, () => console.log('Server is running on localhost:' + PORT)); 
+});
+
