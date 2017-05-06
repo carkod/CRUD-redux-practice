5b3e26675789191e45a6586719f5d4524b25bb88
Carkod <carkodesign@gmail.com>
2017-05-06 01:03:30 +0000

    v

--- src/index.js
+++ src/index.js
@@ -18,9 +18,9 @@ const store = createStore(
 
 ReactDOM.render(
   <BrowserRouter>
-  <Provider store={store}>
-  <App />
-  </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
